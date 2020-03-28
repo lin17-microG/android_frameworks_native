903b4fde350c8aa34eb83f0c2955df8cf93d5666
anupritaisno1 <www.anuprita804@gmail.com>
2020-03-28 13:42:32 +0000

    native: use higher heap sizes for 4/6 GB devices
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- build/phone-xhdpi-4096-dalvik-heap.mk
+++ build/phone-xhdpi-4096-dalvik-heap.mk
@@ -19,7 +19,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.6 \
     dalvik.vm.heapminfree=8m \
     dalvik.vm.heapmaxfree=16m
